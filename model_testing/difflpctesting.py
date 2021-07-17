e2e differentiable lpc (corrected excitation computation bug, working with lpc dump/fixed lpcs)

model_testing/difflpctesting.py
@@ -58,6 +58,8 @@ class diff_pred(Layer):
 
         return tf_l2u(K.sum(pred,axis = 2,keepdims = True))
 
+error_calc = Lambda(lambda x: tf_l2u(tf_u2l(x[0]) - tf.roll(tf_u2l(x[1]),1,axis = 1)))
+
 feature_file = sys.argv[1]
 pcm_file = sys.argv[2]
 
@@ -86,12 +88,12 @@ lpcoeffs = features[:, :, nb_used_features+1:]
 features = features[:, :, :nb_used_features]
 features[:,:,18:36] = 0
 
-list_weightfiles = ['/home/ubuntu/git/LPCNet/model_weights/difflpc/rc_lar_ntt_01.h5']
+list_weightfiles = ['/home/ubuntu/git/LPCNet/model_weights/difflpc/rc_lar_ntt_50.h5']
 
 for file in list_weightfiles:
     dset = file.split('/')[-1].split('_')[2]
     print(dset)
-    np.random.seed(36)
+    np.random.seed(333)
 
     lpc_coeffs = difflpc.difflpc(training=False)
     lpc_coeffs.load_weights(file)
@@ -109,6 +111,7 @@ for file in list_weightfiles:
     model_preds = diff_pred()([inp_mock,lpc_model])
     lpc_model = lpc_model.numpy()
     model_preds = model_preds.numpy()
+    res_delay = error_calc([inp_mock,model_preds])
 
     nauds = 5
     pyp.figure()
@@ -122,24 +125,29 @@ for file in list_weightfiles:
         G_model = (np.linalg.norm(x_framed - ulaw2lin(model_preds[aud_ind,frame_input*frame_size:(frame_input + 1)*frame_size,:])))
         lpc_frame_model = lpc_model[aud_ind,frame_input]
         lpcw_model,lpch_model = freqz(G_model,np.insert(lpc_frame_model,0,1),frame_size//2)
-
-        pyp.subplot(nauds,3,3*i + 1)
+        res_frame = res_delay[aud_ind,frame_input*frame_size:(frame_input + 1)*frame_size,:]
+        
+        pyp.subplot(nauds,4,4*i + 1)
         pyp.title("Spectral Envelopes")
         pyp.plot(np.log(np.abs(np.fft.fft(x_framed[:,0]))[:frame_size//2]),'b')
         pyp.plot(np.log(np.abs(lpch)),'g')
         pyp.plot(np.log(np.abs(lpch_model)),'r')
 
-        pyp.subplot(nauds,3,3*i + 2)
+        pyp.subplot(nauds,4,4*i + 2)
         pyp.title(str(G_gt))
         pyp.plot(x_framed,'b')
         pyp.plot(ulaw2lin(pred[aud_ind,frame_input*frame_size:(frame_input + 1)*frame_size,:]),'g')
         pyp.plot(x_framed - ulaw2lin(pred[aud_ind,frame_input*frame_size:(frame_input + 1)*frame_size,:]),'k')
 
-        pyp.subplot(nauds,3,3*i + 3)
+        pyp.subplot(nauds,4,4*i + 3)
         pyp.title(str(G_model))
         pyp.plot(x_framed,'b')
         pyp.plot(ulaw2lin(model_preds[aud_ind,frame_input*frame_size:(frame_input + 1)*frame_size,:]),'g')
         pyp.plot(x_framed - ulaw2lin(model_preds[aud_ind,frame_input*frame_size:(frame_input + 1)*frame_size,:]),'k')
 
+        pyp.subplot(nauds,4,4*i + 4)
+        pyp.plot((x_framed - ulaw2lin(pred[aud_ind,frame_input*frame_size:(frame_input + 1)*frame_size,:]))[:20],'k')
+        pyp.plot(tf_u2l(res_frame[1:])[:20],'r')
+
     pyp.tight_layout()
-    pyp.savefig('./LPC_plots_' + dset + '_.png')
+    pyp.savefig('./LPC_plots_testing_' + dset + '_.png')

training_tf2/e2edifflpcnet.py
@@ -103,7 +103,8 @@ class Sparsify(Callback):
             
 
 class PCMInit(Initializer):
-    def __init__(self, gain=.1, seed=None):
+    def __init__(self, gain=.1, seed=None,**kwargs):
+        super(PCMInit, self).__init__(**kwargs)
         self.gain = gain
         self.seed = seed
 
@@ -119,7 +120,7 @@ class PCMInit(Initializer):
         #a[:,0] = math.sqrt(12)*np.arange(-.5*num_rows+.5,.5*num_rows-.4)/num_rows
         #a[:,1] = .5*a[:,0]*a[:,0]*a[:,0]
         a = a + np.reshape(math.sqrt(12)*np.arange(-.5*num_rows+.5,.5*num_rows-.4)/num_rows, (num_rows, 1))
-        return self.gain * a
+        return self.gain * a.astype("float32")
 
     def get_config(self):
         return {
@@ -153,13 +154,13 @@ def new_lpcnet_model(rnn_units1=384, rnn_units2=16, nb_used_features = 38, train
     dec_state1 = Input(shape=(rnn_units1,))
     dec_state2 = Input(shape=(rnn_units2,))
     Input_extractor = Lambda(lambda x: K.expand_dims(x[0][:,:,x[1]],axis = -1))
-    error_calc = Lambda(lambda x: tf.roll(x[0],1,axis = -1) - x[1])
+    error_calc = Lambda(lambda x: tf_l2u(tf_u2l(x[0]) - tf.roll(tf_u2l(x[1]),1,axis = 1)))
     padding = 'valid' if training else 'same'
     fconv1 = Conv1D(128, 3, padding=padding, activation='tanh', name='feature_conv1')
     fconv2 = Conv1D(128, 3, padding=padding, activation='tanh', name='feature_conv2')
 
     # Extracting LPCoeffs from Features and obtaining prediction
-    feat2lpc = difflpc.difflpc(training = True)
+    feat2lpc = difflpc.difflpc(training = training)
     lpcoeffs = feat2lpc(feat)
     tensor_preds = diff_pred(name = "lpc2preds")([Input_extractor([pcm,0]),lpcoeffs])
     past_errors = error_calc([Input_extractor([pcm,0]),tensor_preds])
@@ -230,13 +231,16 @@ def new_lpcnet_model(rnn_units1=384, rnn_units2=16, nb_used_features = 38, train
 
 class diff_Embed(Layer):
 
-    def __init__(self, units=128, dict_size = 256, **kwargs):
+    def __init__(self, units=128, dict_size = 256,pcm_init = True, **kwargs):
         super(diff_Embed, self).__init__(**kwargs)
         self.units = units
         self.dict_size = dict_size
+        self.pcm_init = pcm_init
 
     def build(self, input_shape):  # Create the state of the layer (weights)
         w_init = tf.random_normal_initializer()
+        if self.pcm_init:  
+            w_init = PCMInit()
         self.w = tf.Variable(initial_value=w_init(shape=(self.dict_size, self.units),dtype='float32'),trainable=True)
 
     def call(self, inputs):  # Defines the computation from inputs to outputs
@@ -257,6 +261,7 @@ class diff_Embed(Layer):
         config = super(diff_Embed, self).get_config()
         config.update({"units": self.units})
         config.update({"dict_size" : self.dict_size})
+        config.update({"pcm_init" : self.pcm_init})
         return config
 
 scale = 255.0/32768.0

training_tf2/test_e2e_lpcnet.py
@@ -42,7 +42,7 @@ import tensorflow as tf
 # config.gpu_options.per_process_gpu_memory_fraction = 0.2
 # set_session(tf.Session(config=config))
 
-model, enc, dec = lpcnet.new_lpcnet_model()
+model, enc, dec = lpcnet.new_lpcnet_model(training = False)
 
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['sparse_categorical_accuracy'])
 #model.summary()
@@ -64,13 +64,9 @@ features[:,:,18:36] = 0
 periods = (.1 + 50*features[:,:,36:37]+100).astype('int16')
 
 
-dir_w = './model_weights/e2e_fixedlpc/'
+dir_w = './model_weights/e2e_fixedlpc_noninterploss/'
 model.load_weights(dir_w + 'lpcnet33e_384_60.h5')
 
-model_lpcextraction = difflpc.difflpc()
-model_lpcextraction.load_weights('/home/ubuntu/git/LPCNet/model_weights/difflpc/rcplusmse_test_ntt_10.h5')
-layer_name = 'rc2lpc'
-lpc_coeffs = Model(inputs=model_lpcextraction.input,outputs=model_lpcextraction.get_layer(layer_name).output)
 
 order = 16
 
@@ -88,11 +84,10 @@ skip = order + 1
 for c in range(0, nb_frames):
     cfeat,lpcs = enc.predict([features[c:c+1, :, :nb_used_features], periods[c:c+1, :, :]])
     # lpcs = lpc_coeffs([np.zeros((features.shape[0],2400,1)),features[c:c+1, :, :nb_used_features]])[0]
-    # print(features.shape,lpcs.shape)
     for fr in range(0, feature_chunk_size):
         f = c*feature_chunk_size + fr
         # a = features[c, fr, nb_features-order:]
-        a = lpcs[fr]
+        a = lpcs[c,fr]
         for i in range(skip, frame_size):
             pred = -sum(a*pcm[f*frame_size + i - 1:f*frame_size + i - order-1:-1])
             fexc[0, 0, 1] = lin2ulaw(pred)

training_tf2/train_lpcnet.py
@@ -66,6 +66,7 @@ def res_from_sigloss():
         p = y_pred[:,:,0:1]
         model_out = y_pred[:,:,1:]
         e_gt = tf_l2u(tf_u2l(y_true) - tf_u2l(p))
+        e_gt = tf.round(e_gt)
         e_gt = tf.cast(e_gt,'uint8')
         sparse_cel = tf.keras.losses.SparseCategoricalCrossentropy(reduction=tf.keras.losses.Reduction.NONE)(e_gt,model_out)
         return sparse_cel
@@ -87,10 +88,23 @@ def interp_mulaw():
         return loss_mod
     return loss
 
+def metric_icel(y_true, y_pred):
+    p = y_pred[:,:,0:1]
+    model_out = y_pred[:,:,1:]
+    e_gt = tf_l2u(tf_u2l(y_true) - tf_u2l(p))
+    alpha = e_gt - tf.math.floor(e_gt)
+    alpha = tf.tile(alpha,[1,1,256])
+    e_gt = tf.cast(e_gt,'uint8')
+    e_gt = tf.clip_by_value(e_gt,0,254) #Check direction
+    interp_probab = (1 - alpha)*model_out + alpha*tf.roll(model_out,shift = -1,axis = -1)
+    sparse_cel = tf.keras.losses.SparseCategoricalCrossentropy(reduction=tf.keras.losses.Reduction.NONE)(e_gt,interp_probab)
+    return sparse_cel
+
 def metric_cel(y_true, y_pred):
     p = y_pred[:,:,0:1]
     model_out = y_pred[:,:,1:]
     e_gt = tf_l2u(tf_u2l(y_true) - tf_u2l(p))
+    e_gt = tf.round(e_gt)
     e_gt = tf.cast(e_gt,'uint8')
     e_gt = tf.clip_by_value(e_gt,0,255) #Check direction
     sparse_cel = tf.keras.losses.SparseCategoricalCrossentropy(reduction=tf.keras.losses.Reduction.NONE)(e_gt,model_out)
@@ -124,10 +138,10 @@ with strategy.scope():
     model, _, _ = lpcnet.new_lpcnet_model(training=True)
     # model.compile(optimizer=opt, loss='sparse_categorical_crossentropy', metrics=['sparse_categorical_accuracy'])
     model.get_layer("f2lpc").load_weights('/home/ubuntu/git/LPCNet/model_weights/difflpc/rc_lar_ntt_50.h5')
-    for l in range(len(model.get_layer("f2lpc").layers)):
-        model.get_layer("f2lpc").layers[l].trainable = False
+    # for l in range(len(model.get_layer("f2lpc").layers)):
+    #     model.get_layer("f2lpc").layers[l].trainable = False
     # model.get_layer("model").trainable = False
-    model.compile(optimizer=opt, loss=res_from_sigloss(),metrics=[metric_cel,metric_exc_sd])
+    model.compile(optimizer=opt, loss=interp_mulaw(),metrics=[metric_cel,metric_icel,metric_exc_sd])
         # print(l.name, l.trainable)
     # print(model.layers)
     model.summary()
@@ -181,7 +195,7 @@ del pred
 del in_exc
 
 # dump models to disk as we go
-dir_w = './model_weights/e2e_fixedlpc_noninterploss/'
+dir_w = './model_weights/e2e_initlpc_interploss/'
 checkpoint = ModelCheckpoint(dir_w + 'lpcnet33e_384_{epoch:02d}.h5')
 
 if adaptation:
@@ -193,5 +207,5 @@ else:
     sparsify = lpcnet.Sparsify(2000, 40000, 400, (0.05, 0.05, 0.2))
 
 model.save_weights(dir_w + 'lpcnet33e_384_00.h5');
-csv_logger = CSVLogger('e2e_fixedlpc_noninterploss.log')
+csv_logger = CSVLogger('e2e_initlpc_interploss.log')
 model.fit([in_data, features, periods], out_exc, batch_size=batch_size, epochs=nb_epochs, validation_split=0.0, callbacks=[checkpoint, sparsify, csv_logger])